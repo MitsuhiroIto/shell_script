b7d23a2fdba762224d2101cb4ae6cf2afc770fee
mitsuhiro <mitsuhiro.ito@furukawaelectric.com>
2018-02-05 19:36:18 +0900

    create batch_test script

--- batch_test/fetch_and_run.sh
+++ batch_test/fetch_and_run.sh
@@ -2,6 +2,7 @@ export FILE_NAME_AF="profile_canny.jpg"
 export BATCH_FILE_S3_URL_SRC="s3://mitsu-test-batch/profile.jpg"
 export BATCH_FILE_S3_URL_DST="s3://mitsu-test-batch/profile_canny.jpg"
 
-aws s3 cp  $BATCH_FILE_S3_URL_SRC .
-python shell_script/batch_test/image_canny.py $BATCH_FILE_S3_URL_SRC
+aws s3 cp  $BATCH_FILE_S3_URL_SRC ./shell_script/batch_test/
+ls ./shell_script/batch_test/
+python ./shell_script/batch_test/image_canny.py $BATCH_FILE_S3_URL_SRC
 aws s3 cp  $FILE_NAME_AF $BATCH_FILE_S3_URL_DST
