9e0d4e80448aa169fd210b5d96f7976b36e37f3c
mitsuhiro <mitsuhiro.ito@furukawaelectric.com>
2018-02-05 22:36:57 +0900

    create batch_test script

--- batch_test/fetch_and_run.sh
+++ batch_test/fetch_and_run.sh
@@ -1,4 +1,8 @@
+echo $BATCH_FILE_S3_URL_SRC
+echo $FILE_NAME_AF
+echo $BATCH_FILE_S3_URL_DST
 
+pip install awscli
 aws s3 cp  $BATCH_FILE_S3_URL_SRC .
 python ./shell_script/batch_test/image_canny.py $BATCH_FILE_S3_URL_SRC
 aws s3 cp  $FILE_NAME_AF $BATCH_FILE_S3_URL_DST
