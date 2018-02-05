a6590eac54b86cde02486b107f84d6d39bdeebde
mitsuhiro <mitsuhiro.ito@furukawaelectric.com>
2018-02-05 18:43:59 +0900

    create batch_test script

---
+++ batch_test/image_canny.py
@@ -0,0 +1,11 @@
+import sys
+import cv2
+import numpy as np
+
+s3_url = sys.argv[1]
+file_url = s3_url.rsplit('/', 1)[-1]
+im = cv2.imread(file_url)
+im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+im_canny = cv2.Canny(im_gray,100,200)
+save_url = file_url.rsplit('.', 1)[0]  + "_canny." +  file_url.rsplit('.', 1)[1]
+cv2.imwrite(save_url,im_canny)
