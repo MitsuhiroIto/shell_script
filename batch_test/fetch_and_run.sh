596586a1e8a8fea9498326d52c2c1952221cbb12
mitsuhiro <mitsuhiro.ito@furukawaelectric.com>
2018-02-06 22:27:04 +0900

    change batch_test script

--- batch_test/fetch_and_run.sh
+++ batch_test/fetch_and_run.sh
@@ -1,3 +1,9 @@
+PATH="/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin"
+
+export FILE_NAME_AF="profile_canny.jpg"
+export BATCH_FILE_S3_URL_SRC="s3://mitsu-test-batch/profile.jpg"
+export BATCH_FILE_S3_URL_DST="s3://mitsu-test-batch/profile_canny.jpg"
+
 echo $BATCH_FILE_S3_URL_SRC
 aws s3 cp $BATCH_FILE_S3_URL_SRC .
 ls
