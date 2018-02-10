73141762c756ca72b8b5685540ab50a952d6508d
mitsuhiro <mitsuhiro.ito@furukawaelectric.com>
2018-02-11 03:50:28 +0900

    change detect_yolo script

--- detect_yolo/fetch_and_run.sh
+++ detect_yolo/fetch_and_run.sh
@@ -1,9 +1,3 @@
-export FILE_NAME=car.jpg
-export FILE_NAME_AF=car_yolo.jpg
-export BATCH_FILE_S3_URL_SRC=s3://mitsu-aws-image/static/uploads/car.jpg
-export BATCH_FILE_S3_URL_DST=s3://mitsu-aws-image/static/uploads/car_yolo.jpg
-#pip install numpy
-
 export PYTHONPATH=/usr/local/lib/python3.6/site-packages
 
 aws s3 cp $BATCH_FILE_S3_URL_SRC .
