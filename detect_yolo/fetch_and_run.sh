16006f5d11ad0a96060486477126f497f709a93f
mitsuhiro <mitsuhiro.ito@furukawaelectric.com>
2018-02-11 01:11:29 +0900

    change detect_yolo script

--- detect_yolo/fetch_and_run.sh
+++ detect_yolo/fetch_and_run.sh
@@ -1,10 +1,11 @@
 PATH="/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin"
 
-$FILE_NAME = 'car.jpg'
-$FILE_NAME_AF = 'car_yolo.jpg'
-$BATCH_FILE_S3_URL_SRC = 's3://s3://mitsu-zappa-s3-docker/static/uploads/car.jpg'
-$BATCH_FILE_S3_URL_DST = 's3://s3://mitsu-zappa-s3-docker/static/uploads/car_yolo.jpg'
+export FILE_NAME=car.jpg
+export FILE_NAME_AF=car_yolo.jpg
+export BATCH_FILE_S3_URL_SRC=s3://mitsu-aws-image/static/uploads/car.jpg
+export BATCH_FILE_S3_URL_DST=s3://mitsu-aws-image/static/uploads/car_yolo.jpg
 
+RUN python YAD2K/yad2k.py yolo.cfg yolo.weights YAD2K/model_data/yolo.h5
 aws s3 cp $BATCH_FILE_S3_URL_SRC .
 python YAD2K/detect_yolo_under.py $FILE_NAME
 aws s3 cp $FILE_NAME_AF $BATCH_FILE_S3_URL_DST
