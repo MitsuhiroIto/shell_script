dc1f028471f8c3deb03e46eedd1f03d1d898af85
mitsuhiro <mitsuhiro.ito@furukawaelectric.com>
2018-02-11 02:45:26 +0900

    change detect_yolo script

--- detect_yolo/fetch_and_run.sh
+++ detect_yolo/fetch_and_run.sh
@@ -4,7 +4,7 @@ export BATCH_FILE_S3_URL_SRC=s3://mitsu-aws-image/static/uploads/car.jpg
 export BATCH_FILE_S3_URL_DST=s3://mitsu-aws-image/static/uploads/car_yolo.jpg
 #pip install numpy
 
-export PYTHONPATH= /usr/local/lib/python3.6/site-packages
+export PYTHONPATH=/usr/local/lib/python3.6/site-packages
 
 aws s3 cp $BATCH_FILE_S3_URL_SRC .
 python YAD2K/detect_yolo_under.py $FILE_NAME
