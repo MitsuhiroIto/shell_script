4e640a690eb3fa952d9c88951544be752b6505b3
mitsuhiro <mitsuhiro.ito@furukawaelectric.com>
2018-02-12 00:59:08 +0900

    change detect_mask script

--- detect_mask/fetch_and_run.sh
+++ detect_mask/fetch_and_run.sh
@@ -1,9 +1,9 @@
 export PYTHONPATH=/usr/local/lib/python3.6/site-packages
 
-export FILE_NAME=car.jpg
-export FILE_NAME_AF=car_mask.png
-export BATCH_FILE_S3_URL_SRC=s3://mitsu-aws-image/static/uploads/car.jpg
-export BATCH_FILE_S3_URL_DST=s3://mitsu-aws-image/static/uploads/car_mask.png
+# export FILE_NAME=car.jpg
+# export FILE_NAME_AF=car_mask.png
+# export BATCH_FILE_S3_URL_SRC=s3://mitsu-aws-image/static/uploads/car.jpg
+# export BATCH_FILE_S3_URL_DST=s3://mitsu-aws-image/static/uploads/car_mask.png
 
 aws s3 cp $BATCH_FILE_S3_URL_SRC .
 python Mask_RCNN/detect_mask_rcnn.py $FILE_NAME
