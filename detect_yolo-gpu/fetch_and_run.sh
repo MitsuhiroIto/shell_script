a7a210190cc3d1f602dc1b721ce0131404a5fd0e
mitsuhiro <mitsuhiro.ito@furukawaelectric.com>
2018-02-13 14:04:33 +0900

    add detect_yolo-gpu script

---
+++ detect_yolo-gpu/fetch_and_run.sh
@@ -0,0 +1,11 @@
+export PYTHONPATH=/usr/local/lib/python3.6/site-packages
+
+export FILE_NAME=car.jpg
+export FILE_NAME_AF=car_yolo.jpg
+export BATCH_FILE_S3_URL_SRC=s3://mitsu-aws-image/static/uploads/car.jpg
+export BATCH_FILE_S3_URL_DST=s3://mitsu-aws-image/static/uploads/car_yolo.jpg
+
+aws s3 cp $BATCH_FILE_S3_URL_SRC .
+python3 YAD2K/detect_yolo_under.py $FILE_NAME
+aws s3 cp $FILE_NAME_AF $BATCH_FILE_S3_URL_DST
+ls
