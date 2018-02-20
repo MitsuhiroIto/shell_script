1c726a085cd5a537fe96889b9db5b9a9764d3281
mitsuhiro <mitsuhiro.ito@furukawaelectric.com>
2018-02-20 23:26:09 +0900

    add detect_mask-gpu-movie

--- detect_mask-gpu-movie/fetch_and_run.sh
+++ detect_mask-gpu-movie/fetch_and_run.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH=/usr/local/lib/python3.6/site-packages
 
 aws s3 cp $BATCH_FILE_S3_URL_SRC .
-python Mask_RCNN/detect_mask_rcnn_movie.py $FILE_NAME
+python3 Mask_RCNN/detect_mask_rcnn_movie.py $FILE_NAME
 aws s3 cp $FILE_NAME_AF $BATCH_FILE_S3_URL_DST
 ls
